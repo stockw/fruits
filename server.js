ea480adf84dcd461549eb1f46b45fab4aa6a830d
stockw <courtney.wilkins05@gmail.com>
2023-08-16 15:32:56 -0400

    connected to MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const jsxViewEngine = require('jsx-view-engine');
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
 
 const fruits = require('./models/fruits.js');
 const vegatables = require('./models/veggies.js');
 
 const app = express();
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo');
+});
 
 app.set('view engine', 'jsx');
 app.engine('jsx', jsxViewEngine());
